20f5b7ed58d205f432f67325d96f0eea2fc386a1
Firat <firatbicimli@gmail.com>
2023-10-13 08:52:51 +0300

    language style changed

--- src/components/languageMenu.js
+++ src/components/languageMenu.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import i18next from 'i18next'
 import { languages } from '../i18n'
 import tw from 'twin.macro'
@@ -5,6 +6,8 @@ import Link from './link'
 
 const LanguageMenu = () => {
   const { t } = i18next
+
+
 
   return (
     <div tw="flex flex-wrap content-end text-gray-300">
@@ -14,7 +17,7 @@ const LanguageMenu = () => {
             <a
               css={[
                 tw`text-base m-0.5 p-0.5 cursor-pointer uppercase`,
-                i18next.language === lang && tw`text-purple-400 underline `,
+                i18next.language === lang && tw`text-green-400  `,
               ]}
             >
               {t(lang)}
